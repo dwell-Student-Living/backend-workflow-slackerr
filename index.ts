46f05d032a6fb180af809ec3d4acce86736f850c
Alexander Adhyatma <alexander@unblk.io>
2025-04-08 08:07:57 +0700

    feat: initial commit

---
+++ index.ts
@@ -0,0 +1,74 @@
+import {request } from 'https';
+import type {RequestOptions} from 'https';
+import {URL} from 'url';
+
+export default async function sendSlackError(err: Error): Promise<void> {
+  try {
+    const {
+      APP_NAME = 'Unblk Workflow Error Reporter',
+      NODE_ENV = 'development',
+      SLACK_CHANNEL = 'errors',
+      SLACK_ENABLED = 'true',
+    } = process.env;
+
+    if (SLACK_ENABLED !== 'true') {
+      return;
+    }
+
+    let color = 'good';
+    switch (NODE_ENV) {
+      case 'production':
+        color = 'danger';
+        break;
+      case 'staging':
+        color = 'warning';
+        break;
+    }
+
+    const attachment = {
+      fallback: err.message,
+      title: 'Error details:',
+      pretext: `NODE_ENV: ${NODE_ENV}`,
+      //   title_link:
+      //     'https://console.aws.amazon.com/cloudwatch/home?region=us-east-1#logsV2:log-groups',
+      text: err.stack !== undefined ? err.stack : err.message,
+      color,
+    };
+
+    const params = {
+      text: `${APP_NAME}: ${err.message}`,
+      icon_emoji: 'warning',
+      channel: SLACK_CHANNEL,
+      attachments: [attachment],
+    };
+    await makePostRequest(params);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function makePostRequest(data: unknown) {
+  return new Promise((resolve, reject) => {
+    const {SLACK_URL = 'CHANGE_ME'} = process.env;
+    if (SLACK_URL === 'CHANGE_ME') {
+      throw new Error('Please configure SLACK_URL env variable.');
+    }
+    const body = JSON.stringify(data);
+    const {hostname, pathname} = new URL(SLACK_URL);
+    const options: RequestOptions = {
+      hostname,
+      path: pathname,
+      port: 443,
+      method: 'POST',
+      headers: {
+        'Content-Type': 'x-www-form-urlencoded',
+        'Content-Length': body.length,
+      },
+    };
+    const req = request(options, () => resolve(true));
+    req.on('error', error => reject(error));
+    req.on('timeout', () => reject('TIMEOUT'));
+    req.write(body);
+    req.end();
+  });
+}
